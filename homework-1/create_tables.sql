cc9851978c398237395879bab131e1f8e86bacd0
s.leontev <neonyan15@gmail.com>
2023-12-22 16:00:11 +0300

    Done

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,29 @@
 -- SQL-команды для создания таблиц
+CREATE TABLE employees (
+	employee_id SERIAL PRIMARY KEY,
+	first_name VARCHAR(50),
+	last_name VARCHAR(50),
+	title VARCHAR(50),
+	birth_date DATE,
+	notes TEXT
+);
+
+SELECT * FROM employees;
+
+CREATE TABLE customers (
+	customer_id VARCHAR(5) PRIMARY KEY,
+	company_name VARCHAR(50),
+	contact_name VARCHAR(50)
+);
+
+SELECT * FROM customers;
+
+CREATE TABLE orders (
+	order_id SERIAL PRIMARY KEY,
+	customer_id VARCHAR(50),
+	employee_id INT,
+	order_date DATE,
+	ship_city VARCHAR(50)
+);
+
+SELECT * FROM orders;
--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,71 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
+
+import csv
+
+import psycopg2
+
+conn = psycopg2.connect(
+    host= "localhost",
+    database= "north",
+    user= "postgres",
+    password= "68686868"
+)
+
+try:
+    with conn:
+        with conn.cursor() as cursor:
+            with open('north_data/customers_data.csv', 'r') as file:
+                reader = csv.reader(file)
+                next(reader)
+                for row in reader:
+                    cursor.execute(
+"INSERT INTO customers (customer_id, company_name, contact_name) VALUES (%s, %s, %s)",
+row
+)
+
+finally:
+    conn.close()
+
+conn = psycopg2.connect(
+    host= "localhost",
+    database= "north",
+    user= "postgres",
+    password= "68686868"
+)
+
+try:
+    with conn:
+        with conn.cursor() as cursor:
+            with open('north_data/employees_data.csv', 'r') as file:
+                reader = csv.reader(file)
+                next(reader)
+                for row in reader:
+                    cursor.execute(
+"INSERT INTO employees (employee_id, first_name, last_name, title, birth_date, notes) VALUES (%s, %s, %s, %s, %s, %s)",
+row
+)
+
+finally:
+    conn.close()
+
+conn = psycopg2.connect(
+    host= "localhost",
+    database= "north",
+    user= "postgres",
+    password= "68686868"
+)
+
+try:
+    with conn:
+        with conn.cursor() as cursor:
+            with open('north_data/orders_data.csv', 'r') as file:
+                reader = csv.reader(file)
+                next(reader)
+                for row in reader:
+                    cursor.execute(
+"INSERT INTO orders (order_id, customer_id, employee_id, order_date, ship_city) VALUES (%s, %s, %s, %s, %s)",
+row
+)
+
+finally:
+    conn.close()
